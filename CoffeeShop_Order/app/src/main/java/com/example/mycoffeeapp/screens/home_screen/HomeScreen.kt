219442e3c03581945fa76c8a3eaa73c5d867a52d
Harshit Awasthi <74407688+Harshit-Awasthi@users.noreply.github.com>
2026-04-26 02:00:37 +0530

    Added home screen

---
+++ CoffeeShop_Order/app/src/main/java/com/example/mycoffeeapp/screens/home_screen/HomeScreen.kt
@@ -0,0 +1,64 @@
+package com.example.mycoffeeapp.screens.home_screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mycoffeeapp.ui_components.BottomNavBar
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun HomeScreen() {
+
+    val location="Noida 63 - sector"
+
+    Scaffold(
+        bottomBar = { BottomNavBar() }
+    ) { innerPadding ->
+
+        Box(modifier = Modifier.fillMaxWidth().fillMaxHeight(1f/3f)
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFF303030),
+                        Color(0xFF1F1F1F),
+                        Color(0xFF121212)
+                    )
+                )
+            ))
+
+        Column (modifier = Modifier.fillMaxSize().padding(16.dp).padding(innerPadding)){
+            Text(text="Location", color =Color.Gray, fontSize = 14.sp)
+            Spacer(modifier = Modifier.height(4.dp))
+            Row(verticalAlignment = Alignment.CenterVertically)
+            { Text(text = location, color = Color.White, fontWeight = FontWeight.SemiBold, fontSize = 16.sp)
+                Icon(imageVector = Icons.Default.KeyboardArrowDown, contentDescription = "Change Location", tint = Color.White)
+            }
+            Spacer(modifier = Modifier.height(40.dp))
+            SearchBar()
+
+        }
+    }
+}
